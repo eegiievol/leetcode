9953fda15557e71e04295706b82c9bf688dc8663
unknown <eulziisaikhan@miu.edu>
2021-11-21 19:14:05 -0600

    add 347

---
+++ priority_queue/347. Top K Frequent Elements.py
@@ -0,0 +1,31 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []        
+        freq = {}
+        
+        #number frequency
+        for i in nums:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+        
+        
+        #tuple = (ctr, num)
+        #maxheap minus value trick
+        
+        #heappop(heap), heappush(heap, (ctr, num))
+        for el, count in freq.items():
+            heappush(heap, (count*(-1), el))
+
+        #here
+            
+        ans = []
+        for _ in range(k):
+            ans.append(heappop(heap)[1])
+        
+        return ans
+            
+        
+        
+        
