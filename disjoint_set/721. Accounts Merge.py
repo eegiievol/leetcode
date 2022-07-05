c4e93bd511b359ee86a4c99a1c6510ae5097d31f
Erdenebat <eegiievol@gmail.com>
2022-07-04 20:34:52 -0500

    Update 721. Accounts Merge.py

--- disjoint_set/721. Accounts Merge.py
+++ disjoint_set/721. Accounts Merge.py
@@ -1,48 +1,32 @@
-# Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
-
-# Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
-
-# After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
-
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:        
-        def findRoot(email):
-            while parent[email]!=email:
-                email = parent[email]
-            return email
-            
-        def union(e1, e2):
-            r1,r2 = findRoot(e1), findRoot(e2)
-            if height[r1]==height[r2]:
-                parent[r2]=r1
-                height[r1]+=1
-            elif height[r1]>height[r2]:
-                parent[r2]=r1
-            else:
-                parent[r1]=r2
+        def find(acc):
+            if root[acc] == acc:
+                return acc
+            root[acc] = find(root[acc])
+            return root[acc]     
         
-        height = {}
-        parent = {}
-        owner = {}
+        def union(acc1, acc2):
+            r1, r2 = find(acc1), find(acc2)            
+            if r1!=r2:
+                root[r2] = r1                
+        
+        #account owner
+        names = {}
         for account in accounts:
             for email in account[1:]:
-                height[email]=0
-                parent[email]=email
-                owner[email]=account[0]
+                names[email] = account[0]
+        root = {name:name for name in names}
         
+        #find unions
         for account in accounts:
-            for e1,e2 in zip(account[1:], account[2:]):
-                union(e1,e2)
-                
-        register = defaultdict(list)
-        for account in accounts:
-            for email in account[1:]:
-                root = findRoot(email)
-                if email not in register[root]:
-                    register[root].append(email)
-                
-        ans = []
-        for root in register:
-            register[root].sort()
-            ans.append([owner[root]]+register[root])
-        return ans
+            for e1, e2 in zip(account[1:], account[2:]):
+                union(e1, e2)
+        
+        #register accounts in root account
+        reg = defaultdict(list)
+        for email in names:
+            r = find(email)
+            reg[r].append(email)
+        
+        return [[names[acc]] + sorted(reg[acc]) for acc in reg]
