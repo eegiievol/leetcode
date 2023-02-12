bc018530ed476be57dd022c4c1b6ab76d383a648
Erdenebat <eegiievol@gmail.com>
2023-02-12 19:41:42 +0000

    5

--- dp_tabular/__5. Longest Palindromic Substring.py
+++ dp_tabular/__5. Longest Palindromic Substring.py
@@ -17,22 +17,22 @@ Example 2:
 Input: s = "cbbd"
 Output: "bb"
 '''
-
+import collections
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s:
-            return ""
         le = len(s)
-        dp = [[False]*le for _ in range(le)]
+        dp = [[0] * le for _ in range(le)]
         for i in range(le):
-            dp[i][i] = True
-        ans = s[0]
-
-        for i in range(le-2, -1, -1):
-            for j in range(i+1, le):
-                if s[i]==s[j]:
-                    if i+1==j or dp[i+1][j-1]:
-                        dp[i][j] = True
-                        ans = max(s[i:j+1], ans, key=len)
+            dp[i][i] = 1
         
+
+        ans = s[0]
+        for i in range(le-2, -1, -1):
+            for j in range(i, le):
+                if s[i]==s[j]:
+                    if i+1>=j or dp[i+1][j-1]:
+                        dp[i][j] = 1
+                        ans = max(ans, s[i:j+1], key=len)
         return ans
+
+
