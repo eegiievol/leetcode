4e262b0b3c6d83f9d5addafb7db0aa5199dbffa8
Erdenebat <eegiievol@gmail.com>
2023-06-13 05:03:40 +0000

    aa

--- topological_sort/207. Course Schedule.py
+++ topological_sort/207. Course Schedule.py
@@ -12,27 +12,52 @@
 # Explanation: There are a total of 2 courses to take. 
 # To take course 1 you should have finished course 0. So it is possible.
 
+#SOLUTION 1: Grey Node Algorithm for Cycle detect
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:        
-        indegree = [0 for _ in range(numCourses)]
-        adj_list = defaultdict(list)
+        def dfs(node):
+            if status[node]==1: #grey node
+                return False
+            if status[node]==2:
+                return True
+            
+            status[node]=1
+            for nei in neighbors[node]:
+                if not dfs(nei):
+                    return False
+            status[node]=2
+            return True
         
-        for s,d in prerequisites:
-            indegree[d]+=1
-            adj_list[s].append(d)   
+        status = {i:0 for i in range(numCourses)}
+        neighbors = defaultdict(list)
+        for c,p in prerequisites:
+            neighbors[p].append(c)
         
-        q = deque()
         for i in range(numCourses):
-            if indegree[i]==0:
-                q.append(i)
-                
-        order = []
+            if not dfs(i):
+                return False
+        return True
+
+#SOLUTION 2: Topological sort algorithm for Cycle detect
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        neighbors = defaultdict(list)
+        indegree = [0 for _ in range(numCourses)]
+        for c,p in prerequisites:
+            indegree[c] += 1
+            neighbors[p].append(c)
+        q = deque()
+        for c in range(numCourses):
+            if indegree[c]==0: q.append(c)
+        
+        #topological sort algorithm
+        coursesTaken = 0
         while q:
-            popped = q.popleft()
-            order.append(popped)
-            for nei in adj_list[popped]:
-                indegree[nei]-=1
-                if indegree[nei]==0:
+            course = q.popleft()
+            coursesTaken += 1
+            for nei in neighbors[course]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0: 
                     q.append(nei)
         
-        return True if len(order)==numCourses else False
+        return True if coursesTaken == numCourses else False
